0dbd393bd2a832993c25abe28593a5ee5b17e266
Gokul Soumya <gokulps15@gmail.com>
2023-12-21 02:21:43 +0530

    Consistent indentation for test files

--- impl/tests/corpus/literals.scm
+++ impl/tests/corpus/literals.scm
@@ -1,36 +1,36 @@
 (test-suite "literals"
 
   (test-case "true literal"
-   :program
-     "true"
-   :untyped-ast
-     (BoolLiteral :value True)
-   :typed-ast
-     (BoolLiteral Bool))
+    :program
+      "true"
+    :untyped-ast
+      (BoolLiteral :value True)
+    :typed-ast
+      (BoolLiteral Bool))
 
   (test-case "false literal"
-   :program
-     "false"
-   :untyped-ast
-     (BoolLiteral :value False)
-   :typed-ast
-     (BoolLiteral Bool))
+    :program
+      "false"
+    :untyped-ast
+      (BoolLiteral :value False)
+    :typed-ast
+      (BoolLiteral Bool))
 
   (test-case "positive int literal"
-   :program
-     "123"
-   :untyped-ast
-     (IntLiteral :value "123")
-   :typed-ast
-     (IntLiteral Int))
+    :program
+      "123"
+    :untyped-ast
+      (IntLiteral :value "123")
+    :typed-ast
+      (IntLiteral Int))
 
   (test-case "negative int literal"
-   :program
-     "-1"
-   :untyped-ast
-     (UnaryOp
-      :op "-"
-      :operand (IntLiteral :value "1"))
-   :typed-ast
-     (UnaryOp Int
-      :operand (IntLiteral Int))))
+    :program
+      "-1"
+    :untyped-ast
+      (UnaryOp
+        :op "-"
+        :operand (IntLiteral :value "1"))
+    :typed-ast
+      (UnaryOp Int
+        :operand (IntLiteral Int))))
