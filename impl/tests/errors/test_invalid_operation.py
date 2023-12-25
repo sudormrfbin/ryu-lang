1b86024f3b18cb7b109dcd062754a71ea667ca76
Gokul Soumya <gokulps15@gmail.com>
2023-12-25 20:19:48 +0530

    Add test cases for binary operation type errors

--- impl/tests/errors/test_invalid_operation.py
+++ impl/tests/errors/test_invalid_operation.py
@@ -21,3 +21,47 @@ def test_unary_op_bool_error():
             assert span.coord() == ((1, 2), (1, 6))
         case _:
             assert False
+
+
+def test_binary_op_bool_error():
+    with pytest.raises(InvalidOperationError) as excinfo:
+        ast = parse_tree_to_ast(parse("true+false"))
+        #                              123456789
+        ast.typecheck()
+
+    err = excinfo.value
+
+    assert err.span.coord() == ((1, 1), (1, 11))
+    assert err.operator.name == "+"
+    assert err.operator.span.coord() == ((1, 5), (1, 6))
+
+    match err.operands:
+        case [OperandSpan(type_=t1, span=s1), OperandSpan(type_=t2, span=s2)]:
+            assert t1.name == "Bool"
+            assert s1.coord() == ((1, 1), (1, 5))
+            assert t2.name == "Bool"
+            assert s2.coord() == ((1, 6), (1, 11))
+        case _:
+            assert False
+
+
+def test_binary_op_bool_interror():
+    with pytest.raises(InvalidOperationError) as excinfo:
+        ast = parse_tree_to_ast(parse("24+false"))
+        #                              123456789
+        ast.typecheck()
+
+    err = excinfo.value
+
+    assert err.span.coord() == ((1, 1), (1, 9))
+    assert err.operator.name == "+"
+    assert err.operator.span.coord() == ((1, 3), (1, 4))
+
+    match err.operands:
+        case [OperandSpan(type_=t1, span=s1), OperandSpan(type_=t2, span=s2)]:
+            assert t1.name == "Int"
+            assert s1.coord() == ((1, 1), (1, 3))
+            assert t2.name == "Bool"
+            assert s2.coord() == ((1, 4), (1, 9))
+        case _:
+            assert False
