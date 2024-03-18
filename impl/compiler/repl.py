c2c828da383c9327650787576fbdc3b328fbc1ce
Kiran Pradeep <90208563+kiranpradeepk@users.noreply.github.com>
2024-03-18 10:05:12 +0530

    Support multiline input in repl

--- impl/compiler/repl.py
+++ impl/compiler/repl.py
@@ -8,7 +8,7 @@ def repl() -> None:
 
     while True:
         try:
-            source = input(">> ").strip()
+            source = read_statement()
         except (KeyboardInterrupt, EOFError):
             print("\nExiting")
             break
@@ -19,3 +19,24 @@ def repl() -> None:
         result = run(source, type_env, runtime_env)
         if result is not None:
             print(result)
+
+def read_statement():
+    source = input(">> ").strip()
+
+    while not scope_is_balanced(source):
+        source += input(".. ").strip()
+
+    return source
+
+def scope_is_balanced(string: str) -> bool:
+    stack = []
+
+    for char in string:
+        if char == '{':
+            stack.append(char)
+        elif char == '}':
+            if not stack:
+                return False  # Found a closing brace without a corresponding opening brace
+            stack.pop()
+    
+    return not stack  # If stack is empty, braces are balanced
