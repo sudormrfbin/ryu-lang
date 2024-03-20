69bcaea8cab257b67a0485510176d7791794c7a5
Gokul Soumya <gokulps15@gmail.com>
2024-03-20 10:51:01 +0530

    Fix multiline input in repl

--- impl/compiler/repl.py
+++ impl/compiler/repl.py
@@ -20,23 +20,27 @@ def repl() -> None:
         if result is not None:
             print(result)
 
+
 def read_statement():
-    source = input(">> ").strip()
+    source = input(">> ")
 
     while not scope_is_balanced(source):
-        source += input(".. ").strip()
+        source += "\n" + input(".. ")
 
     return source
 
+
 def scope_is_balanced(string: str) -> bool:
-    stack = []
+    stack: list[str] = []
 
     for char in string:
-        if char == '{':
+        if char == "{":
             stack.append(char)
-        elif char == '}':
+        elif char == "}":
             if not stack:
-                return False  # Found a closing brace without a corresponding opening brace
+                return (
+                    False  # Found a closing brace without a corresponding opening brace
+                )
             stack.pop()
-    
+
     return not stack  # If stack is empty, braces are balanced
