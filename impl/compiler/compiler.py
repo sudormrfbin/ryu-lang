69585a8d09fdc47769aa484ebed94048ab8b1a7e
Gokul Soumya <gokulps15@gmail.com>
2024-03-22 04:39:58 +0530

    Use compiler error's report function to display errors

--- impl/compiler/compiler.py
+++ impl/compiler/compiler.py
@@ -1,49 +1,16 @@
-from typing import TYPE_CHECKING, Any
-
-# error_report submodule is generated dynamically by pyo3 on the rust
-# side. Hence pyright cannot detect the source file.
-from error_report.error_report import (  # pyright: ignore [reportMissingModuleSource]
-    report_error,
-)
+from typing import Any
 
 from compiler.env import RuntimeEnvironment, TypeEnvironment
 
 from compiler import errors
-from compiler.errors import (
-    InvalidOperationError,
-    OperandSpan,
-    UndeclaredVariable,
-    UnexpectedType,
-    UnknownVariable,
-)
 from compiler.parser import parse, parse_tree_to_ast
-
-if TYPE_CHECKING:
-    from error_report.error_report import (  # pyright: ignore [reportMissingModuleSource]
-        Mark,
-        Message,
-    )
 
 
 def run(source: str, type_env: TypeEnvironment, runtime_env: RuntimeEnvironment) -> Any:
     try:
         return _run(source, type_env, runtime_env)
-    except errors.InvalidOperationError as err:
-        err.report(source)
-    except errors.UnknownVariable as err:
-        err.report(source)
-    except errors.UndeclaredVariable as err:
-        err.report(source)
-    except errors.UnexpectedType as err:
-        err.report(source)
-    except errors.TypeMismatch as err:
-        err.report(source)
-    except errors.DuplicatedCase as err:
-        err.report(source)
-    except errors.InexhaustiveMatch as err:
-        err.report(source)
     except errors.CompilerError as err:
-        print(err)
+        err.report(source)
 
 
 def _run(
