036849da3b2678c8f506afde21dfad9a62e58dbe
Gokul Soumya <gokulps15@gmail.com>
2024-03-24 19:58:12 +0530

    Formatting changes

--- impl/tests/test_index_assignment.py
+++ impl/tests/test_index_assignment.py
@@ -1,6 +1,18 @@
 from compiler.env import RuntimeEnvironment, TypeEnvironment
 from compiler.parser import parse, parse_tree_to_ast
-from compiler.ast import ArrayElement, ArrayElements, ArrayLiteral, Assignment, IndexAssignment, Indexing, IntLiteral, StatementList, Term, Variable, VariableDeclaration
+from compiler.ast import (
+    ArrayElement,
+    ArrayElements,
+    ArrayLiteral,
+    Assignment,
+    IndexAssignment,
+    Indexing,
+    IntLiteral,
+    StatementList,
+    Term,
+    Variable,
+    VariableDeclaration,
+)
 from compiler.langtypes import INT, Array, Block, Int
 from tests.utils import docstring_source
 
@@ -12,15 +24,76 @@ def test_index_assignment(source: str):
     x[1]=4
     """
     ast = parse_tree_to_ast(parse(source))
-    assert ast.to_dict() =={StatementList: {'stmts': [{VariableDeclaration: {'ident': 'x', 'rvalue': {ArrayLiteral: {'members': {ArrayElements: {'members': [{ArrayElement: {'element': {IntLiteral: {'value': 2}}}}, {ArrayElement: {'element': {IntLiteral: {'value': 3}}}}]}}}}}}, {IndexAssignment: {'arrayname': {Variable: {'value': 'x'}}, 'index': 1, 'value': {IntLiteral: {'value': 4}}}}]}}
+    assert ast.to_dict() == {
+        StatementList: {
+            "stmts": [
+                {
+                    VariableDeclaration: {
+                        "ident": "x",
+                        "rvalue": {
+                            ArrayLiteral: {
+                                "members": {
+                                    ArrayElements: {
+                                        "members": [
+                                            {
+                                                ArrayElement: {
+                                                    "element": {
+                                                        IntLiteral: {"value": 2}
+                                                    }
+                                                }
+                                            },
+                                            {
+                                                ArrayElement: {
+                                                    "element": {
+                                                        IntLiteral: {"value": 3}
+                                                    }
+                                                }
+                                            },
+                                        ]
+                                    }
+                                }
+                            }
+                        },
+                    }
+                },
+                {
+                    IndexAssignment: {
+                        "arrayname": {Variable: {"value": "x"}},
+                        "index": 1,
+                        "value": {IntLiteral: {"value": 4}},
+                    }
+                },
+            ]
+        }
+    }
     type_env = TypeEnvironment()
     ast.typecheck(type_env)
-    print( ast.to_type_dict())
-    assert ast.to_type_dict() =={StatementList: Block, 'stmts': [{VariableDeclaration: Array, 'rvalue': {ArrayLiteral: Array, 'members': {ArrayElements: Array, 'members': [{ArrayElement: Int, 'element': {IntLiteral: Int}}, {ArrayElement: Int, 'element': {IntLiteral: Int}}]}}}, {IndexAssignment: Array, 'arrayname': {Variable: Array}, 'value': {IntLiteral:Int}}]}
+    print(ast.to_type_dict())
+    assert ast.to_type_dict() == {
+        StatementList: Block,
+        "stmts": [
+            {
+                VariableDeclaration: Array,
+                "rvalue": {
+                    ArrayLiteral: Array,
+                    "members": {
+                        ArrayElements: Array,
+                        "members": [
+                            {ArrayElement: Int, "element": {IntLiteral: Int}},
+                            {ArrayElement: Int, "element": {IntLiteral: Int}},
+                        ],
+                    },
+                },
+            },
+            {
+                IndexAssignment: Array,
+                "arrayname": {Variable: Array},
+                "value": {IntLiteral: Int},
+            },
+        ],
+    }
     assert type_env.get("x") == Array(INT)
 
     env = RuntimeEnvironment()
     ast.eval(env)
-    assert env.get("x") == [2,4]
-
-    
+    assert env.get("x") == [2, 4]
--- impl/tests/test_indexing.py
+++ impl/tests/test_indexing.py
@@ -1,6 +1,17 @@
 from compiler.env import RuntimeEnvironment, TypeEnvironment
 from compiler.parser import parse, parse_tree_to_ast
-from compiler.ast import ArrayElement, ArrayElements, ArrayLiteral, Assignment, Indexing, IntLiteral, StatementList, Term, Variable, VariableDeclaration
+from compiler.ast import (
+    ArrayElement,
+    ArrayElements,
+    ArrayLiteral,
+    Assignment,
+    Indexing,
+    IntLiteral,
+    StatementList,
+    Term,
+    Variable,
+    VariableDeclaration,
+)
 from compiler.langtypes import INT, Array, Block, Int
 from tests.utils import docstring_source
 
@@ -12,11 +23,77 @@ def test_indexing(source: str):
     let b=x[1]
     """
     ast = parse_tree_to_ast(parse(source))
- 
-    assert ast.to_dict() =={StatementList: {'stmts': [{VariableDeclaration: {'ident':  'x', 'rvalue': {ArrayLiteral: {'members': {ArrayElements: {'members': [{ArrayElement: {'element': {IntLiteral: {'value': 2}}}}, {ArrayElement: {'element': {IntLiteral: {'value': 3}}}}]}}}}}}, {VariableDeclaration: {'ident':  'b', 'rvalue': {Indexing: {'element': {Variable: {'value':  'x'}}, 'index': 1}}}}]}}
+
+    assert ast.to_dict() == {
+        StatementList: {
+            "stmts": [
+                {
+                    VariableDeclaration: {
+                        "ident": "x",
+                        "rvalue": {
+                            ArrayLiteral: {
+                                "members": {
+                                    ArrayElements: {
+                                        "members": [
+                                            {
+                                                ArrayElement: {
+                                                    "element": {
+                                                        IntLiteral: {"value": 2}
+                                                    }
+                                                }
+                                            },
+                                            {
+                                                ArrayElement: {
+                                                    "element": {
+                                                        IntLiteral: {"value": 3}
+                                                    }
+                                                }
+                                            },
+                                        ]
+                                    }
+                                }
+                            }
+                        },
+                    }
+                },
+                {
+                    VariableDeclaration: {
+                        "ident": "b",
+                        "rvalue": {
+                            Indexing: {
+                                "element": {Variable: {"value": "x"}},
+                                "index": 1,
+                            }
+                        },
+                    }
+                },
+            ]
+        }
+    }
     type_env = TypeEnvironment()
     ast.typecheck(type_env)
-    assert ast.to_type_dict() =={StatementList: Block, 'stmts': [{VariableDeclaration: Array, 'rvalue': {ArrayLiteral: Array, 'members': {ArrayElements: Array, 'members': [{ArrayElement: Int, 'element': {IntLiteral: Int}}, {ArrayElement: Int, 'element': {IntLiteral: Int}}]}}}, {VariableDeclaration: Array, 'rvalue': {Indexing: Array, 'element': {Variable: Array}}}]}
+    assert ast.to_type_dict() == {
+        StatementList: Block,
+        "stmts": [
+            {
+                VariableDeclaration: Array,
+                "rvalue": {
+                    ArrayLiteral: Array,
+                    "members": {
+                        ArrayElements: Array,
+                        "members": [
+                            {ArrayElement: Int, "element": {IntLiteral: Int}},
+                            {ArrayElement: Int, "element": {IntLiteral: Int}},
+                        ],
+                    },
+                },
+            },
+            {
+                VariableDeclaration: Array,
+                "rvalue": {Indexing: Array, "element": {Variable: Array}},
+            },
+        ],
+    }
     assert type_env.get("x") == Array(INT)
 
     env = RuntimeEnvironment()
