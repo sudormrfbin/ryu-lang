0d02bd6aab9fdf89a83d0c72db5676c34d6e5c98
Gokul Soumya <gokulps15@gmail.com>
2024-03-30 11:05:21 +0530

    Cleanup array element type mismatch error

--- impl/tests/errors/test_array_elements_type_mismatch.py
+++ impl/tests/errors/test_array_elements_type_mismatch.py
@@ -11,20 +11,20 @@ EMPTY_ENV = RuntimeEnvironment()
 EMPTY_TYPE_ENV = TypeEnvironment()
 
 
-def test_if_stmt_not_bool_cond():
+def test_array_elements_type_mismatch():
     with pytest.raises(ArrayTypeMismatch) as excinfo:
-        ast = parse_tree_to_ast(parse("let x=[1,true]"))
+        ast = parse_tree_to_ast(parse("let x = [1, true]"))
         ast.typecheck(EMPTY_TYPE_ENV)
 
     err = excinfo.value
 
-    assert err.span.coord() == ((1, 10), (1, 14))
-    assert err.expected_type_span.coord() == ((1, 8), (1, 9))
+    assert err.span.coord() == ((1, 13), (1, 17))
+    assert err.expected_type_span.coord() == ((1, 10), (1, 11))
     assert err.expected_type == INT
     assert err.actual_type == BOOL
 
 
-def test_only_variable_output(capfd: CaptureFixture[str], snapshot: str):
-    run("let x=[1,true]", EMPTY_TYPE_ENV, EMPTY_ENV)
+def test_array_elements_type_mismatch_output(capfd: CaptureFixture[str], snapshot: str):
+    run("let x = [1, true]", EMPTY_TYPE_ENV, EMPTY_ENV)
     _, err = capfd.readouterr()
     assert snapshot == err
