65bcec0c2af51ef89a877c2e52673915131d8b2b
Gokul Soumya <gokulps15@gmail.com>
2024-03-30 11:57:28 +0530

    Cleanup index array assignment error

--- impl/tests/errors/test_array_index_assignment_type_mismatch.py
+++ impl/tests/errors/test_array_index_assignment_type_mismatch.py
@@ -11,28 +11,27 @@ EMPTY_ENV = RuntimeEnvironment()
 EMPTY_TYPE_ENV = TypeEnvironment()
 
 
+SOURCE = """\
+let a = [1, 3, 5]
+a[2] = true
+"""
+
+
 def test_if_stmt_not_bool_cond():
     with pytest.raises(ArrayIndexAssignmentTypeMismatch) as excinfo:
-        ast = parse_tree_to_ast(
-            parse(
-                """let a=[1,3,5]
-                                         a[2]=true"""
-            )
-        )
+        ast = parse_tree_to_ast(parse(SOURCE))
         ast.typecheck(EMPTY_TYPE_ENV)
         ast.eval(EMPTY_ENV)
 
     err = excinfo.value
 
-    assert err.span.coord() == ((2, 47), (2, 51))
+    assert err.span.coord() == ((2, 8), (2, 12))
+    assert err.actual_type == BOOL
+    assert err.expected_type == INT
+    assert err.expected_type_span.coord() == ((2, 1), (2, 2))
 
 
 def test_only_variable_output(capfd: CaptureFixture[str], snapshot: str):
-    run(
-        """let a=[1,3,5]
-           a[2]=true""",
-        EMPTY_TYPE_ENV,
-        EMPTY_ENV,
-    )
+    run(SOURCE, EMPTY_TYPE_ENV, EMPTY_ENV)
     _, err = capfd.readouterr()
     assert snapshot == err
