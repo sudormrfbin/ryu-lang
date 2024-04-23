36447ce8e4031c6ed4fddb2fbec3b6c357ace389
Gokul Soumya <gokulps15@gmail.com>
2024-04-23 10:42:25 +0530

    Move statements to separate file

--- impl/compiler/ast/__init__.py
+++ impl/compiler/ast/__init__.py
@@ -1,4 +1,3 @@
-import abc
 from typing import Any, Optional, TypeAlias
 from dataclasses import dataclass
 from typing_extensions import override
@@ -22,46 +21,15 @@ from compiler.matcher import (
 )
 
 from compiler.ast.literals import StringLiteral as StringLiteral
+from compiler.ast.statements import (
+    StatementBlock,
+    Statement,
+    StatementList as StatementList,
+)
 
 
 # TODO: Narrow down this type
 EvalResult = Any
-
-
-class Statement(Ast):
-    @abc.abstractmethod
-    def typecheck(self, env: TypeEnvironment):
-        pass
-
-    @abc.abstractmethod
-    def eval(self, env: RuntimeEnvironment):
-        pass
-
-
-@dataclass
-class StatementList(Ast):
-    stmts: list[Statement]
-
-    def typecheck(self, env: TypeEnvironment):
-        for stmt in self.stmts:
-            stmt.typecheck(env)
-
-    def eval(self, env: RuntimeEnvironment):
-        for child in self.stmts:
-            child.eval(env)
-
-
-@dataclass
-class StatementBlock(StatementList):
-    @override
-    def typecheck(self, env: TypeEnvironment):
-        child_env = TypeEnvironment(enclosing=env)
-        super().typecheck(child_env)
-
-    @override
-    def eval(self, env: RuntimeEnvironment):
-        child_env = RuntimeEnvironment(enclosing=env)
-        return super().eval(child_env)
 
 
 @dataclass
