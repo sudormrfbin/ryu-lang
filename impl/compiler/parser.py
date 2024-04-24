cc78f7c60a36551c1aca80758cfdf3988fca3294
Gokul Soumya <gokulps15@gmail.com>
2024-04-24 08:41:14 +0530

    Reorder lark transformer methods

--- impl/compiler/parser.py
+++ impl/compiler/parser.py
@@ -40,59 +40,74 @@ class LarkTreeToAstTransformer(Transformer[Token, Any]):
 
     statement_list = listify(ast.statements.StatementList)
     statement_block = listify(ast.statements.StatementBlock)
+
     variable_declaration = ast.variable.VariableDeclaration
+    variable = ast.variable.Variable
     assignment = ast.variable.Assignment
+
+    array_literal = ast.array.ArrayLiteral
+    array_elements = listify(ast.array.ArrayElements)
+    array_element = ast.array.ArrayElement
     index_assignment = ast.array.IndexAssignment
+    indexing = ast.array.Indexing
+
     print_stmt = ast.print.PrintStmt
+
     if_chain = ast.if_stmt.IfChain
     if_stmt = ast.if_stmt.IfStmt
     else_if_ladder = listify(ast.if_stmt.ElseIfLadder)
     else_if_stmt = ast.if_stmt.ElseIfStmt
+
     match_stmt = ast.match.MatchStmt
     case_ladder = listify(ast.match.CaseLadder)
     case_stmt = ast.match.CaseStmt
     enum_pattern = ast.match.EnumPattern
     enum_pattern_tuple = ast.match.EnumPatternTuple
+    array_pattern = listify(ast.match.ArrayPattern)
+    array_pattern_element = ast.match.ArrayPatternElement
     wildcard_pattern = ast.match.WildcardPattern
+
     while_stmt = ast.loops.WhileStmt
     for_stmt = ast.loops.ForStmt
     for_stmt_int = ast.loops.ForStmtInt
+
     struct_stmt = ast.struct.StructStmt
     struct_members = listify(ast.struct.StructMembers)
     struct_member = ast.struct.StructMember
     struct_access = ast.struct.StructAccess
     struct_assignment = ast.struct.StructAssignment
+
+    struct_init_members = listify(ast.struct.StructInitMembers)
+    struct_init_member = ast.struct.StructInitMember
+
     enum_stmt = ast.enum.EnumStmt
     enum_members = listify(ast.enum.EnumMembers)
     enum_member_bare = ast.enum.EnumMemberBare
     enum_member_tuple = ast.enum.EnumMemberTuple
+
+    enum_literal_simple = ast.enum.EnumLiteralSimple
+    enum_literal_tuple = ast.enum.EnumLiteralTuple
+
     function_definition = ast.function.FunctionDefinition
     function_params = listify(ast.function.FunctionParams)
     function_param = ast.function.FunctionParam
-    type_annotation = ast.annotation.TypeAnnotation
     return_stmt = ast.function.ReturnStmt
+
+    function_call = ast.function.FunctionCall
+    function_args = listify(ast.function.FunctionArgs)
+
+    type_annotation = ast.annotation.TypeAnnotation
+
     equality = ast.operators.Equality
     logical = ast.operators.Logical
     comparison = ast.operators.Comparison
     term = ast.operators.Term
     factor = ast.operators.Factor
     unary_op = ast.operators.UnaryOp
-    indexing = ast.array.Indexing
-    function_call = ast.function.FunctionCall
-    function_args = listify(ast.function.FunctionArgs)
-    struct_init_members = listify(ast.struct.StructInitMembers)
-    struct_init_member = ast.struct.StructInitMember
+
     bool_literal = ast.literals.BoolLiteral
     int_literal = ast.literals.IntLiteral
     string_literal = ast.literals.StringLiteral
-    enum_literal_simple = ast.enum.EnumLiteralSimple
-    enum_literal_tuple = ast.enum.EnumLiteralTuple
-    variable = ast.variable.Variable
-    array_literal = ast.array.ArrayLiteral
-    array_elements = listify(ast.array.ArrayElements)
-    array_element = ast.array.ArrayElement
-    array_pattern = listify(ast.match.ArrayPattern)
-    array_pattern_element = ast.match.ArrayPatternElement
 
 
 _transformer = LarkTreeToAstTransformer()
